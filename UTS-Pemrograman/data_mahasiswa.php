47ecdc667245fd9cfc0ab9f0afad37a9e5332abe
shin962 <121811508+shin962@users.noreply.github.com>
2023-05-29 19:14:58 +0800

    Update data_mahasiswa.php

--- UTS-Pemrograman/data_mahasiswa.php
+++ UTS-Pemrograman/data_mahasiswa.php
@@ -8,7 +8,7 @@ include_once('konfigurasi.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=a, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Mahasiswa</title>
 </head>
 <body>
 
