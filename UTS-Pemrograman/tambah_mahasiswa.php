bb254ffce12db01db88982c2e111a0bc8a32cc43
shin962 <121811508+shin962@users.noreply.github.com>
2023-05-29 19:15:51 +0800

    Update tambah_mahasiswa.php

--- UTS-Pemrograman/tambah_mahasiswa.php
+++ UTS-Pemrograman/tambah_mahasiswa.php
@@ -32,7 +32,7 @@ if(isset($_POST["tambah"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tambah Mahasiswa</title>
 </head>
 <body>
     <form action="tambah_mahasiswa.php" method="POST">
